5033bff5f51106e30f1b5f0ce89a268c50f36e33
Mahdibena100102 <mahdibenadjal100102@gmail.com>
2024-10-01 14:53:17 +0100

    Created add,list,module & view screens, also a screen stlye (#1)
    Co-authored-by: mahdi <k2009406@kingston.ac.uk>

---
+++ src/components/screens/ModuleAddScreen.js
@@ -0,0 +1,13 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Screen from "../layout/Screen";
+
+export default ModuleAddScreen = () => {
+  return (
+    <Screen>
+      <Text>Add</Text>
+    </Screen>
+  );
+};
+
+const styles = StyleSheet.create({});
